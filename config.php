ee35d2cca582c2a8ba2ef0ee6c8d46ab5fef4666
Evgeniy `f0t0n` Naydenov <evgeniy.naydonov@gmail.com>
2012-01-18 20:48:25 +0200

    example config

--- config.php
+++ config.php
@@ -1,7 +1,5 @@
 <?php
 return array(
-	//'apiKey'=>'6742e4fB629e8d6f686E0F6c196e2F3b',
-	//'projectID'=>'pid_11C6E31ab424CA375CB23774CCbB3eba',
-	'apiKey'=>'255BAe4a62Ac831e19895b122a013e4E',
-	'projectID'=>'pid_740b47f9FC579d6FbDD87034Fa099910',
+	'apiKey'=>'YOUR_COMPANY_API_KEY_HERE',
+	'projectID'=>'YOR_PROJECT_ID',
 );
